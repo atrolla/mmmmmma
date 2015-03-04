9e8623a6bead09e4bb78127665bca9062b599477
atrolla <yvan.vu@arolla.fr>
2015-03-04 23:37:40 +0100

    get First Bot Character instead of getting first character which could be a player so will not respond to commands.

--- core/test/org/atrolla/game/core/GameTests.java
+++ core/test/org/atrolla/game/core/GameTests.java
@@ -26,11 +26,13 @@ import static org.junit.Assert.assertNotEquals;
 public class GameTests {
 
     private Game defaultGame;
+    private GameCharacter firstBot;
 
     @Before
     public void setUp() throws Exception {
         defaultGame = new Game();
         defaultGame.setControllers(new Array<>());
+        firstBot = defaultGame.getCharacters().stream().filter(c -> !c.isPlayer()).findFirst().get();
     }
 
     @Test
@@ -65,13 +67,12 @@ public class GameTests {
 
     @Test
     public void outOfBoundsCharStaysInBound() throws Exception {
-        final GameCharacter gameCharacter = (GameCharacter) defaultGame.getCharacters().toArray()[0];
         for (int i = 0; i < 10000; i++) {
-            gameCharacter.moves(Direction.DOWN);
+            firstBot.moves(Direction.DOWN);
         }
-        assertTrue(defaultGame.getStage().isOutOfBound(gameCharacter));
+        assertTrue(defaultGame.getStage().isOutOfBound(firstBot));
         defaultGame.update();
-        assertFalse(defaultGame.getStage().isOutOfBound(gameCharacter));
+        assertFalse(defaultGame.getStage().isOutOfBound(firstBot));
     }
 
     @Test
@@ -91,16 +92,15 @@ public class GameTests {
 
     @Test
     public void botIsNotStuckOnBounds() throws Exception {
-        final GameCharacter gameCharacter = (GameCharacter) defaultGame.getCharacters().toArray()[0];
         // put char out of bound
         for (int i = 0; i < 10000; i++) {
-            gameCharacter.moves(Direction.DOWN);
+            firstBot.moves(Direction.DOWN);
         }
         // put direction that makes it still out of bound
         Command command = new Command(Direction.DOWN_RIGHT,500);
         defaultGame.getAIManager().getCommands().set(0,command);
         defaultGame.update();
-        assertFalse(defaultGame.getStage().isOutOfBound(gameCharacter));
+        assertFalse(defaultGame.getStage().isOutOfBound(firstBot));
         final Command generatedCommand = defaultGame.getAIManager().getCommands().get(0);
         assertNotEquals(Direction.DOWN, generatedCommand.getDirection());
         assertNotEquals(Direction.DOWN_RIGHT, generatedCommand.getDirection());
@@ -109,36 +109,34 @@ public class GameTests {
 
     @Test
     public void characterHitStopMove() throws Exception {
-        final GameCharacter gameCharacter = (GameCharacter) defaultGame.getCharacters().toArray()[0];
-        final Coordinates baseCoordinates = gameCharacter.getCoordinates();
+        final Coordinates baseCoordinates = firstBot.getCoordinates();
         Command command = new Command(Direction.DOWN_RIGHT,500);
-        defaultGame.getAIManager().getCommands().set(0,command);
+        defaultGame.getAIManager().getCommands().set(0, command);
         defaultGame.update();
-        final Coordinates coordinates2 = gameCharacter.getCoordinates();
+        final Coordinates coordinates2 = firstBot.getCoordinates();
         assertNotEquals(baseCoordinates, coordinates2);
-        gameCharacter.hit();
+        firstBot.hit();
         defaultGame.update();
-        assertEquals(coordinates2, gameCharacter.getCoordinates());
+        assertEquals(coordinates2, firstBot.getCoordinates());
     }
 
     @Test
     public void knockOutBotMovesAfterSomeTime() throws Exception {
-        final GameCharacter gameCharacter = (GameCharacter) defaultGame.getCharacters().toArray()[0];
         Command command = new Command(Direction.DOWN_RIGHT,500);
         defaultGame.getAIManager().getCommands().set(0,command);
         defaultGame.update();
-        final Coordinates baseCoordinates = gameCharacter.getCoordinates();
-        Assert.assertTrue(gameCharacter.canMove());
-        gameCharacter.hit();
-        assertFalse(gameCharacter.canMove());
+        final Coordinates baseCoordinates = firstBot.getCoordinates();
+        Assert.assertTrue(firstBot.canMove());
+        firstBot.hit();
+        assertFalse(firstBot.canMove());
         int i = 0;
         while (i++ < ConfigurationConstants.BOT_KNOCK_OUT_DURATION) {
             defaultGame.update();
-            assertFalse(gameCharacter.canMove());
-            assertEquals(baseCoordinates, gameCharacter.getCoordinates());
+            assertFalse(firstBot.canMove());
+            assertEquals(baseCoordinates, firstBot.getCoordinates());
         }
         defaultGame.update();
-        assertTrue(gameCharacter.canMove());
-        assertNotEquals(baseCoordinates, gameCharacter.getCoordinates());
+        assertTrue(firstBot.canMove());
+        assertNotEquals(baseCoordinates, firstBot.getCoordinates());
     }
 }
